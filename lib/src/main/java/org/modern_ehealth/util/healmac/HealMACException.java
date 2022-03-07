521a2018a8f57d7a376a883f9473ac89672f3857
Marianne Whitacre <mwhitacre@modern-ehealth.org>
2022-03-07 15:01:50 -0800

    Javadoc comments for HealMAC class

--- lib/src/main/java/org/modern_ehealth/util/healmac/HealMACException.java
+++ lib/src/main/java/org/modern_ehealth/util/healmac/HealMACException.java
@@ -1,5 +1,11 @@
 package org.modern_ehealth.util.healmac;
 
+/**
+ * Exception thrown by HealMAC methods when an expectation is violated.
+ *
+ * @author Dorin Jinks
+ * @since 1.0
+ */
 public class HealMACException extends RuntimeException {
     public HealMACException(String message) {
         super(message);
